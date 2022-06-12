46dc55d67a6723bb59c304ea5ef253fe58dd062a
ma-oliveiramarques <ma_oliveiramarques@hotmail.com>
2022-06-12 01:54:01 +0100

    added footer section
    aula 3

--- main.js
+++ main.js
@@ -18,16 +18,18 @@ for (const link of links) {
 }
 
 //mudar o header da página quando der scroll
-const header = document.querySelector('#header')
-const navHeight = header.offsetHeight
+function changeHeaderWhenScroll() {
+  const header = document.querySelector('#header')
+  const navHeight = header.offsetHeight
 
-window.addEventListener('scroll', function () {
   if (window.scrollY >= navHeight) {
+    //scrol é maior do que a altura do header
     header.classList.add('scroll')
   } else {
+    //menor que a altura do header
     header.classList.remove('scroll')
   }
-})
+}
 
 // Testimonials carousel slider swiper
 const swiper = new Swiper('.swiper-container', {
@@ -59,6 +61,24 @@ scrollReveal.reveal(
   #services header, #services .card, 
   #testimonials header, #testimonials .testimonials
   #contact .text, #contact .links
+  footer .brand, footer .social
   `,
   { interval: 100 }
 )
+
+// Botão voltar para o topo
+function backToTop() {
+  const backToTopButton = document.querySelector('.back-to-top')
+
+  if (window.scrollY >= 560) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+
+// When Scroll
+window.addEventListener('scroll', function () {
+  changeHeaderWhenScroll()
+  backToTop()
+})
